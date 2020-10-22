16030ca327d6d5c46e20b97d72e264a2f4fbb8bb
mrpriyansh <mr_priyanshgaharana@gmail.com>
2020-10-22 13:01:31 +0530

    download_xml

--- src/component/Bar/Bar.js
+++ src/component/Bar/Bar.js
@@ -1,41 +1,73 @@
 import React from 'react';
-import styles from './Bar.module.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import SelectSearch from 'react-select-search';
+import styles from './Bar.module.css';
 import './styles.css';
 import { useGlobal } from '../../services/context';
-function Bar () {
-    // const options = [
-    //     {name: 'Swedish', value: 'sv'},
-    //     {name: 'English', value: 'en'},
-    //     {
-    //         type: 'group',
-    //         name: 'Group name',
-    //         items: [
-    //             {name: 'Spanish', value: 'es'},
-    //         ]
-    //     },
-    // ];
-    const {response, setUrl, setData} = useGlobal();
-    const options = response.pages.map((page, i)=>{
-        return {name: `${page.title}(${page.url}) `, value: i }
-    });
-    const onOptionChange =(e) =>{
-        const {url, parent, title} = response.pages[e];
-        setUrl(url);
-        if(response.mapping[url])
-            setData(response.mapping[url]);
-        else setData({children:[], title, parent })
-    }
-    return (
-        <div className={styles.bar}>
-            <Link to="/" className={styles.title}>Home</Link>
-            <span className={styles.website}> {response.url} </span>
-            <div className={styles.searchBox}>
-                <SelectSearch options={options} search={true} name="language" placeholder="Search Page" onChange={onOptionChange}/>
-            </div>
+import { ReactComponent as XMLICON } from '../../assets/icons/xml.svg';
+import config from '../../services/config';
+
+const fileDownload = require('js-file-download');
+
+function Bar() {
+  // const options = [
+  //     {name: 'Swedish', value: 'sv'},
+  //     {name: 'English', value: 'en'},
+  //     {
+  //         type: 'group',
+  //         name: 'Group name',
+  //         items: [
+  //             {name: 'Spanish', value: 'es'},
+  //         ]
+  //     },
+  // ];
+  const { response, setUrl, setData } = useGlobal();
+  const options = response.pages.map((page, i) => {
+    return { name: `${page.title}(${page.url}) `, value: i };
+  });
+  const generateXML = e => {
+    console.log('a');
+    fetch(`${config.apiUrl}/api/generatexml`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ pages: response.pages }),
+    })
+      .then(resp => resp.json().then(data => ({ status: resp.status, body: data })))
+      .then(res => {
+        console.log(res);
+        fileDownload(res.body, `${response.url}.xml`);
+      });
+  };
+  const onOptionChange = e => {
+    const { url, parent, title } = response.pages[e];
+    setUrl(url);
+    if (response.mapping[url]) setData(response.mapping[url]);
+    else setData({ children: [], title, parent });
+  };
+  return (
+    <div className={styles.bar}>
+      <Link to="/" className={styles.title}>
+        Home
+      </Link>
+      <div className={styles.website_wrapper}>
+        <span className={styles.website}> {response.url} </span>
+        <div className={styles.xml_icon_wrapper}>
+          <XMLICON className={styles.xml_icon} fill="#fff" onClick={generateXML} />
+          <span className={styles.overlay}>Download Sitemap!</span>
         </div>
-    )
+        {/* <img src={xmlIcon} className={styles.xml_icon} alt="xmlPNG" onClick={generateXML} /> */}
+      </div>
+      <div className={styles.searchBox}>
+        <SelectSearch
+          options={options}
+          search={true}
+          name="language"
+          placeholder="Search Page"
+          onChange={onOptionChange}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Bar;
--- src/component/Landing/Landing.js
+++ src/component/Landing/Landing.js
@@ -3,56 +3,71 @@ import { useHistory } from 'react-router';
 import styles from './Landing.module.css';
 
 function Landing() {
-    const [inputUrl, changeInputUrl] = useState('https://mika.house/');
-    const [npages, setNpages] = useState(1);
-    const history = useHistory();
-    const handleSubmit = e => {
-        e.preventDefault();
-        // history.push(`/processing/?url=asdf`)
-        history.push(`/processing/?url=${inputUrl}&npages=${npages}`)
-        // fetch(`${config.apiUrl}/api/scrap`, {
-        //     method:'POST', 
-        //     headers: {'Content-Type': 'application/json'},
-        //     body: JSON.stringify({url:inputUrl}),
-        // })
-        // .then(response => response.json().then(data => ({ status: response.status, body: data })))
-        // .then(res=>{
-        //     setResponse(res.body);
-        //     setData(res.body.mapping[res.body.url]);
-        //     setUrl(res.body.url);
-        //    history.push({
-        //     pathname: '/processing/'
-        //    })
-        //     // if(res.status===200){
-                
-        //     // }
-        // })
-    }
-    const onChangeURl = e => {
-        changeInputUrl(e.target.value);
-    }
-    const onChangePageCount = e =>{
-        setNpages(e.target.value);
-    }
-    return (
-        <div className={styles.landing}>
-            <div className={styles.text_wrapper}>
-                <p className={styles.text_1}>
-                    This website helps you to generate 
-                </p>
-                <p className={styles.text_2}>
-                    user-friendly sitemaps of websties you love.
-                </p>
-            </div>
-            <form onSubmit={handleSubmit} className={styles.form}>
-                <div className={styles.wrapper}>
-                    <input type="url" className={styles.input_field} required placeholder="Enter domain url" value={inputUrl} onChange={onChangeURl}/>
-                    <input type="number" min="0"  className={styles.input_number} required placeholder="Max page" value={npages} onChange={onChangePageCount} />
-                </div>
-                <button type="submit" className={styles.generate} > Generate</button>
-            </form>
+  const [inputUrl, changeInputUrl] = useState('https://mika.house/');
+  const [npages, setNpages] = useState();
+  const history = useHistory();
+  const handleSubmit = e => {
+    e.preventDefault();
+    // history.push(`/processing/?url=asdf`)
+    history.push(`/processing/?url=${inputUrl}&npages=${npages}`);
+    // fetch(`${config.apiUrl}/api/scrap`, {
+    //     method:'POST',
+    //     headers: {'Content-Type': 'application/json'},
+    //     body: JSON.stringify({url:inputUrl}),
+    // })
+    // .then(response => response.json().then(data => ({ status: response.status, body: data })))
+    // .then(res=>{
+    //     setResponse(res.body);
+    //     setData(res.body.mapping[res.body.url]);
+    //     setUrl(res.body.url);
+    //    history.push({
+    //     pathname: '/processing/'
+    //    })
+    //     // if(res.status===200){
+
+    //     // }
+    // })
+  };
+  const onChangeURl = e => {
+    changeInputUrl(e.target.value);
+  };
+  const onChangePageCount = e => {
+    setNpages(e.target.value);
+  };
+  return (
+    <div className={styles.landing}>
+      <div className={styles.text_wrapper}>
+        <p className={styles.text_1}>This website helps you to generate</p>
+        <p className={styles.text_2}>user-friendly sitemaps of websties you love.</p>
+      </div>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <div className={styles.wrapper}>
+          <input
+            type="url"
+            className={styles.input_field}
+            required
+            placeholder="Enter domain url"
+            value={inputUrl}
+            onChange={onChangeURl}
+          />
+          <input
+            type="number"
+            min="1"
+            max="500"
+            className={styles.input_number}
+            required
+            placeholder="Max pages"
+            value={npages}
+            onChange={onChangePageCount}
+          />
         </div>
-    )
+        <button type="submit" className={styles.generate}>
+          {' '}
+          Generate
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Landing;
